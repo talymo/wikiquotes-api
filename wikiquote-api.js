a11a59dc7c47d419be43fc794d1e4206c001dd90
Tabetha Moe <icreatetheinternet@gmail.com>
2015-07-23 14:48:39 -0400

    Update wikiquote-api.js

--- wikiquote-api.js
+++ wikiquote-api.js
@@ -117,15 +117,17 @@ var WikiquoteApi = (function() {
       },
 
       success: function(result, status){
-        console.log(result);
+        
         var quotes = result.parse.text["*"];
         var quoteArray = []
 
-        console.log(quotes);
         // Find top level <li> only
         var $lis = $(quotes).find('li:not(li li)');
+        
         $lis.each(function() {
           // Remove all children that aren't <b>
+          var author = $(this).find('ul li a').html();
+          console.log(author);
           $(this).children().remove(':not(b)');
           var $bolds = $(this).find('b');
 
